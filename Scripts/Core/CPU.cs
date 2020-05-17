13afb7228e2f7a95cc3987d0d1767df4183b7710
Monczak <szymon.jedrzejczak.sj@gmail.com>
2020-05-17 14:06:49 +0200

    Add instruction decoder

--- Scripts/Core/CPU.cs
+++ Scripts/Core/CPU.cs
@@ -97,10 +97,179 @@ namespace Crispy.Scripts.Core
         {
             switch (opcode & 0xF000)
             {
+                case 0x0000:
+                    if ((opcode & 0x00F0) == 0x00E0)
+                    {
+                        switch (opcode & 0x000F)
+                        {
+                            case 0x0000:    // 0x00E0
+                                Opcode_ClearScreen();
+                                break;
+                            case 0x000E:    // 0x00EE
+                                Opcode_ReturnFromSubroutine();
+                                break;
+                            default:
+                                ThrowUnknownOpcodeException();
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Opcode_Call((ushort)(opcode & 0x0FFF));
+                    }
+                    break;
+
+                case 0x1000:
+                    Opcode_JumpToAddress((ushort)(opcode & 0x0FFF));
+                    break;
+
+                case 0x2000:
+                    Opcode_CallSubroutine((ushort)(opcode & 0x0FFF));
+                    break;
+
+                case 0x3000:
+                    Opcode_SkipNextInstructionEqualToValue((byte)((opcode & 0x0F00) >> 8), (byte)(opcode & 0x00FF));
+                    break;
+
+                case 0x4000:
+                    Opcode_SkipNextInstructionNotEqualToValue((byte)((opcode & 0x0F00) >> 8), (byte)(opcode & 0x00FF));
+                    break;
+
+                case 0x5000:
+                    Opcode_SkipNextInstructionXEqualToY((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4));
+                    break;
+
+                case 0x6000:
+                    Opcode_Set((byte)((opcode & 0x0F00) >> 8), (byte)(opcode & 0x00FF));
+                    break;
+
+                case 0x7000:
+                    Opcode_Add((byte)((opcode & 0x0F00) >> 8), (byte)(opcode & 0x00FF));
+                    break;
+
+                case 0x8000:
+                    switch (opcode & 0x000F)
+                    {
+                        case 0x0000:    // 8XY0
+                            Opcode_SetXY((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4));
+                            break;
+                        case 0x0001:    // 8XY1
+                            Opcode_SetXORY((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4));
+                            break;
+                        case 0x0002:    // 8XY2
+                            Opcode_SetXANDY((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4));
+                            break;
+                        case 0x0003:    // 8XY3
+                            Opcode_SetXXORY((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4));
+                            break;
+                        case 0x0004:    // 8XY4
+                            Opcode_AddCarry((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4));
+                            break;
+                        case 0x0005:    // 8XY5
+                            Opcode_SubtractCarry((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4));
+                            break;
+                        case 0x0006:    // 8XY6
+                            Opcode_RightShift((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x0007:    // 8XY7
+                            Opcode_SetXY((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4));
+                            break;
+                        case 0x000E:    // 8XYE
+                            Opcode_LeftShift((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        default:
+                            ThrowUnknownOpcodeException();
+                            break;
+                    }
+                    break;
+
+                case 0x9000:
+                    if ((opcode & 0x000F) != 0x0000)
+                    {
+                        ThrowUnknownOpcodeException();
+                        break;
+                    }
+
+                    Opcode_SkipNextInstructionXNotEqualY((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4));
+                    break;
+
+                case 0xA000:
+                    Opcode_SetIndexRegister((ushort)(opcode & 0x0FFF));
+                    break;
+
+                case 0xB000:
+                    Opcode_JumpToAddressV0((ushort)(opcode & 0x0FFF));
+                    break;
+
+                case 0xC000:
+                    Opcode_SetRandom((byte)((opcode & 0x0F00) >> 8), (byte)(opcode & 0x00FF));
+                    break;
+
+                case 0xD000:
+                    Opcode_Draw((byte)((opcode & 0x0F00) >> 8), (byte)((opcode & 0x00F0) >> 4), (byte)(opcode & 0x000F));
+                    break;
+
+                case 0xE000:
+                    switch (opcode & 0x00FF)
+                    {
+                        case 0x009E:    // EX9E
+                            Opcode_SkipNextInstructionKeyPressed((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x00A1:    // EXA1
+                            Opcode_SkipNextInstructionKeyNotPressed((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        default:
+                            ThrowUnknownOpcodeException();
+                            break;
+                    }
+                    break;
+
+                case 0xF000:
+                    switch (opcode & 0x00FF)
+                    {
+                        case 0x0007:    // FX07
+                            Opcode_SetXToDelayTimer((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x000A:    // FX0A
+                            Opcode_WaitForKeyPress((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x0015:    // FX15
+                            Opcode_SetDelayTimer((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x0018:    // FX18
+                            Opcode_SetSoundTimer((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x001E:    // FX1E
+                            Opcode_AddXToIndexRegister((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x0029:    // FX29
+                            Opcode_SetIndexRegisterSprite((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x0033:    // FX33
+                            Opcode_StoreBCD((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x0055:    // FX55
+                            Opcode_StoreRegisters((byte)((opcode & 0x0F00) >> 8));
+                            break;
+                        case 0x0065:    // FX65
+                            Opcode_ReadRegisters((byte)((opcode & 0x0F00) >> 8));
+                            break;
+
+                        default:
+                            ThrowUnknownOpcodeException();
+                            break;
+                    }
+                    break;
 
                 default:
-                    throw new Exception($"Unknown opcode 0x{opcode.ToString("X4")} at 0x{programCounter.ToString("X3")}");
+                    ThrowUnknownOpcodeException();
+                    break;
             }
+        }
+
+        private void ThrowUnknownOpcodeException()
+        {
+            throw new Exception($"Unknown opcode 0x{opcode.ToString("X4")} at 0x{programCounter.ToString("X3")}");
         }
 
         private void UpdateTimers()
@@ -156,7 +325,7 @@ namespace Crispy.Scripts.Core
         }
 
         // Skip next instruction if VX == VY (0x5XY0)
-        private void Opcode_SkipNextInstructionXEqualY(byte registerX, byte registerY)
+        private void Opcode_SkipNextInstructionXEqualToY(byte registerX, byte registerY)
         {
 
         }
@@ -221,14 +390,14 @@ namespace Crispy.Scripts.Core
 
         }
 
-        // Left shift VX by 1 (0x8XY8)
+        // Left shift VX by 1 (0x8XYE)
         private void Opcode_LeftShift(byte registerX)
         {
 
         }
 
         // Skip next instruction if VX != VY (0x9XY0)
-        private void SkipNextInstructionXNotEqualY(byte registerX, byte registerY)
+        private void Opcode_SkipNextInstructionXNotEqualY(byte registerX, byte registerY)
         {
 
         }
@@ -281,8 +450,14 @@ namespace Crispy.Scripts.Core
 
         }
 
-        // Set delay timer to VX (0xFX18)
+        // Set delay timer to VX (0xFX15)
         private void Opcode_SetDelayTimer(byte register)
+        {
+
+        }
+
+        // Set delay timer to VX (0xFX18)
+        private void Opcode_SetSoundTimer(byte register)
         {
 
         }
